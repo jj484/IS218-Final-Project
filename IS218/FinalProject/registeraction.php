b145bd57564b9a0b92a29373f720442cc318509d
jj484 <jj484@njit.edu>
2021-05-03 19:56:59 -0400

    testing database queries

--- IS218/FinalProject/registeraction.php
+++ IS218/FinalProject/registeraction.php
@@ -1,4 +1,18 @@
 <?php
-    header("Location: http://www.youtube.com");
-    exit;
+require('config.php');
+
+$conn_string = "mysql:host=$host;dbname=$database;charset=utf8mb4";
+$db = new PDO($conn_string, $username, $password);
+
+$stmt = $db->query("SELECT * FROM TestUsers WHERE username='lew'");
+$result = $stmt->fetch();
+
+print_r($result);
+
+/*
+$tableName = "LOL";
+$createQuery = "CREATE TABLE $tableName (username VARCHAR(16), score INT(16) )";
+$db->exec($createQuery);
+*/
+
 ?>
